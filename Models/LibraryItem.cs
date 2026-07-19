77ffda8a51e2f6e59a898f4c13c641e0dcc0a6d2
donia123163 <donhub000@gmail.com>
2026-07-19 22:08:07 +0300

    Add abstract LibraryItem class

---
+++ Models/LibraryItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public abstract class LibraryItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime AddedDate { get; set; }
+        public LibraryItem()
+        {
+            AddedDate = DateTime.Now;
+        }
+        public abstract string GetInfo();
+    }
+}
