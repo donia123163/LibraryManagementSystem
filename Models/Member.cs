09311f201e4e202502e0b07c6aa9ed717c858d09
donia123163 <donhub000@gmail.com>
2026-07-19 23:10:13 +0300

    implement member and premiumMember classes

---
+++ Models/Member.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibraryManagementSystem.Interfaces;
+namespace LibraryManagementSystem.Models
+{
+    public class Member : ISearchable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime JoinDate { get; set; }
+        public Book[] BorrowedBooks { get; set; }
+        public Member()
+        {
+            JoinDate = DateTime.Now;
+            BorrowedBooks = new Book[10];
+        }
+        public virtual string GetInfo()
+        {
+            return $"ID: {Id} | Name: {Name} | Email: {Email}";
+        }
+        public bool MatchesQuery(string query)
+        {
+            query = query.ToLower();
+            return Name.ToLower().Contains(query)
+                || Email.ToLower().Contains(query);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Models/PremiumMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class PremiumMember : Member
+    {
+        public int MaxBorrowLimit { get; set; }
+        public int LoanDays { get; set; }
+        public PremiumMember()
+        {
+            MaxBorrowLimit = 10;
+            LoanDays = 30;
+        }
+        public override string GetInfo()
+        {
+            return $"[Premium] {base.GetInfo()} | Max Borrow: {MaxBorrowLimit} | Loan Days: {LoanDays}";
+        }
+    }
+}
