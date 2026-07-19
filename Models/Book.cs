dedd254221b8f57f1866ce732385ca62a8a7a9a4
donia123163 <donhub000@gmail.com>
2026-07-19 22:20:57 +0300

    implement Book class

---
+++ Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibraryManagementSystem.Interfaces;
+namespace LibraryManagementSystem.Models
+{
+    public class Book : LibraryItem, ISearchable
+    {
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public bool IsAvailable { get; set; }
+        public Book()
+        {
+            IsAvailable = true;
+        }
+        public override string GetInfo()
+        {
+            return $"ID: {Id} | {Title} | {Author} | {Year} | {Genre} | {(IsAvailable ? "Available" : "Borrowed")}";
+        }
+        public bool MatchesQuery(string query)
+        {
+            query = query.ToLower();
+            return Title.ToLower().Contains(query)
+                || Author.ToLower().Contains(query)
+                || Genre.ToLower().Contains(query);
+        }
+    }
+}
